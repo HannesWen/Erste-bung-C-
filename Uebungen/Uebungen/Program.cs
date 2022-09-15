130ef9424bf59898b4fba857c9eeef88ba130703
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-15 13:47:59 +0200

    insert an amount of y to fill the given word to 20 letters

--- Uebungen/Uebungen/Program.cs
+++ Uebungen/Uebungen/Program.cs
@@ -11,14 +11,12 @@ namespace Uebungen
         static void Main(string[] args)
         {
             //Aufgabe 1: 
-
             //int zahl1 = 1;
             //int zahl2 = 2;
             //Console.WriteLine(zahl1 + zahl2);
             //Console.ReadLine();
 
             //Aufgabe 2: 
-
             //Console.WriteLine("Welche Zahlen möchtest du Addieren? ");
             //int zahl1 = Convert.ToInt32(Console.ReadLine());
             //int zahl2 = Convert.ToInt32(Console.ReadLine());
@@ -27,7 +25,6 @@ namespace Uebungen
             //Console.ReadLine();
 
             //Aufgabe 3: 
-
             //Console.WriteLine("Gib mir bitte einen Text");
             //string entry = Console.ReadLine();
             //string replaced_entry = entry.Replace("U", "u").Replace("G", "g");
@@ -35,22 +32,23 @@ namespace Uebungen
             //Console.ReadLine();
 
             //Aufgabe 4:
-
             //Console.WriteLine("Wie lautet dein Wunschtext?");
             //string textlänge = Console.ReadLine();
             //Console.WriteLine("Die länge deines textes ist: " + textlänge.Length);
             //Console.ReadLine();
 
             //Aufgabe 2b:
-
-            Console.WriteLine("Bitte gebe mir 2 Zahlen mit Kommastelle");
-            double zahl1 = Convert.ToDouble(Console.ReadLine());
-            double zahl2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(zahl1 + zahl2);
-            Console.ReadLine();
-
-
+            //Console.WriteLine("Bitte gebe mir 2 Zahlen mit Kommastelle");
+            //double zahl1 = Convert.ToDouble(Console.ReadLine());
+            //double zahl2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(zahl1 + zahl2);
+            //Console.ReadLine();
             
+            //Aufgabe 5:
+            Console.WriteLine("Gib mir bitte einen Text!");
+            string text = Console.ReadLine();
+            Console.WriteLine(text.PadLeft(20, 'y'));
+            Console.ReadLine();
         }
     }
 }
