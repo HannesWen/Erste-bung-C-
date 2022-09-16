f320232f3af438642beae19093abe51df7e3ef3f
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-16 09:10:07 +0200

    Seite 2, Aufgabe 4 + 4b

--- Uebungen/Uebungen/Program.cs
+++ Uebungen/Uebungen/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
@@ -10,192 +11,31 @@ namespace Uebungen
     {
         static void Main(string[] args)
         {
-            //Aufgabe 1: 
-            //int zahl1 = 1;
-            //int zahl2 = 2;
-            //Console.WriteLine(zahl1 + zahl2);
-            //Console.ReadLine();
+            Console.WriteLine("Wie viele Einträge möchtest du in deinem int Array?");
 
-            //Aufgabe 2: 
-            //Console.WriteLine("Welche Zahlen möchtest du Addieren? ");
-            //int zahl1 = Convert.ToInt32(Console.ReadLine());
-            //int zahl2 = Convert.ToInt32(Console.ReadLine());
-            //int summe = zahl1 + zahl2;
-            //Console.WriteLine("Die Summe deiner Zahlen ist: " + summe);
-            //Console.ReadLine();
+            int arraygröße = Convert.ToInt16(Console.ReadLine());
+            int[] zahlen = new int [arraygröße] ;
 
-            //Aufgabe 3: 
-            //Console.WriteLine("Gib mir bitte einen Text");
-            //string entry = Console.ReadLine();
-            //string replaced_entry = entry.Replace("U", "u").Replace("G", "g");
-            //Console.WriteLine(replaced_entry);    
-            //Console.ReadLine();
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine("Gebe mir eine Zahl für das Array!");
+                zahlen[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            //Aufgabe 4:
-            //Console.WriteLine("Wie lautet dein Wunschtext?");
-            //string textlänge = Console.ReadLine();
-            //Console.WriteLine("Die länge deines textes ist: " + textlänge.Length);
-            //Console.ReadLine();
+            Console.Write("[ ");
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.Write($" {zahlen[i]} | ");
+            }
+            Console.WriteLine("] ");
 
-            //Aufgabe 2b:
-            //Console.WriteLine("Bitte gebe mir 2 Zahlen mit Kommastelle");
-            //double zahl1 = Convert.ToDouble(Console.ReadLine());
-            //double zahl2 = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine(zahl1 + zahl2);
-            //Console.ReadLine();
-
-            //Aufgabe 5:
-            //Console.WriteLine("Gib mir bitte einen Text!");
-            //string text = Console.ReadLine();
-            //Console.WriteLine(text.PadLeft(20, 'y'));
-            //Console.ReadLine();
-
-            //Aufgabe 6:
-            //Console.WriteLine("Gib mir bitte einen Text!");
-            //string text = Console.ReadLine();
-            //Console.WriteLine(text.PadRight(30, 'y'));
-            //Console.ReadLine();
-
-            //Aufgabe 7:
-            //Console.WriteLine("Bitte gebe mir eine Texteingabe mit einigen Leerzeichen davor und danach, Danke!");
-            //string text = Console.ReadLine();
-            //Console.WriteLine(text);
-            //Console.WriteLine(text.Trim());
-            //Console.ReadLine();
-
-            //Aufgabe 8:
-            //Console.WriteLine("Bitte gib mir einen Text mit ; Danke");
-            //string text = Console.ReadLine();
-            //string[] newText = text.Split(';');
-            //foreach (var word in newText)
-            //{
-            //    Console.WriteLine(word);
-            //}
-            //Console.ReadLine();
-
-            //Aufgabe 9:
-            //Console.WriteLine("Bitte gib mir einen Text deiner Wahl");
-            //string text = Console.ReadLine();
-            //Console.WriteLine(text.Contains("abc"));
-            //Console.ReadLine();
-
-            //if (text.Contains("abc"))
-            //{
-            //    Console.WriteLine("true");
-            //}
-            //else
-            //    Console.WriteLine("false");
-
-            //Aufgabe 10:
-            //Console.WriteLine("bitte gib mir einen text deiner wahl");
-            //string text = Console.ReadLine();
-            //Console.WriteLine(text.ToUpper());
-            //Console.ReadLine();
-
-            //Seite 2, Aufgabe 1
-            //Console.WriteLine("Gib mir bitte eine Zahl");
-            //int zahl = Convert.ToInt32(Console.ReadLine());
-            //int sum = 0;
-            //for (int zahl1 = 0; zahl > sum; zahl1++)
-            //{
-            //    sum = sum + zahl1;
-            //    Console.WriteLine(sum);
-            //}
-            //Console.ReadLine();
-
-            //Seite 2, Aufgabe 2
-            //Console.WriteLine("Gib mir bitte eine Zahl");
-            //int zahl = Convert.ToInt32(Console.ReadLine());
-            //for (int zahl1 = zahl; zahl1 >= 0; zahl1--)
-            //{
-            //    Console.WriteLine(zahl1);
-            //}
-            //Console.ReadLine();
-
-
-            //Seite 2, Aufgabe 3:
-            //Console.WriteLine("Enter a search value");
-            //string searchValue = Console.ReadLine();
-
-            //string namen = "Jonas Bundschuh, Philipp Ehringer ,Niklas Gottermann , Marcello Greulich ,  Andreas Litt ,  Robin Mohr ,  Robert Pfeilmayer ,  Marie Scholz ,  Leon Schuster ,  Erwin Schäberle ,  Jan Throm ,  Mona Walter ,  lukas Welker ,  Johannes Wengert";
-            //string[] list = namen.Split(',');
-
-            //for (int i = 0; i < list.Length; i++)
-            //{
-            //    if (list[i].Contains(searchValue))
-            //    {
-            //        Console.WriteLine(list[i].Trim());
-            //    }
-            //}
-            //Console.ReadKey();
-
-
-            //Zusatzaufgabe
-            //Lasse eine Zahl zwischen 1 und 100 generieren.
-            //Der Nutzer bekommt hinweise ob die Zahl höher oder tiefer liegt. 
-
-            //Random r = new Random();
-            //int random_zahl = r.Next(1, 100);
-            ////Console.WriteLine(random_zahl);
-            //int gesuchte_zahl = 0;
-            //Console.WriteLine("Wir suchen eine zahl zwischen 1 und 100, welche ist es?");
-            //do
-            //{
-            //    gesuchte_zahl = Convert.ToInt32(Console.ReadLine());
-            //    if (gesuchte_zahl == 42 && random_zahl == 42)
-            //    {
-            //        Console.WriteLine("Deine Antwort ist doppelt richtig!"); 
-            //    }
-            //    else if (gesuchte_zahl == 42)
-            //    {
-            //        Console.WriteLine("Deine Antwort ist richtig aber wir suchen eine andere Zahl, versuchs weiter!");
-            //    }
-            //    else if (gesuchte_zahl < random_zahl)
-            //    {
-            //        Console.WriteLine("Die gesuchte Zahl ist größer");
-            //    }
-            //    else if (gesuchte_zahl > random_zahl)
-            //    {
-            //        Console.WriteLine("Die gesuchte Zahl ist kleiner");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Glückwunsch, du hast die richtige Zahl erraten!");
-            //    }
-            //} while (gesuchte_zahl != random_zahl);
-            //Console.ReadKey();
-
-
-            // Wie funktionieren Arrays? 
-
-            //int[] zahlen = new int[5];
-            //[   |   |   |   |   ]
-            //int[] zahlen2 = new int[] { 2, 8, 12, 3, 6 };
-            //[ 2 | 8 | 12 | 3 | 6 ]
-
-            //for (int i = 0; i < zahlen.Length; i++)
-            //{
-            //    Console.WriteLine("Gebe mir eine Zahl für das Array!");
-            //    zahlen[i] = Convert.ToInt32(Console.ReadLine());
-            //}
-            //Console.Write("[ ");
-            //for (int i = 0; i < zahlen.Length; i++)
-            //{
-            //    Console.Write($" {zahlen[i]} | ");
-            //}
-            //Console.Write("] ");
-            //Console.ReadKey();
-
-            //string[] namen = new string[13];
-            //string[] namen2 = new string[]
-            //{
-            //    "Jonas", "Philipp" ,"Niklas" , "Marcello" ,  "Andreas" ,  "Robin" ,  "Robert" ,  "Marie" ,  "Leon" ,  "Erwin" ,  "Jan" ,  "Mona" ,  "lukas" ,  "Johannes"
-            //};
-            //foreach(string trainee in namen2)
-            //{
-            //    Console.Write(trainee);
-            //}
-            //Console.ReadKey();
+            double durchschnitt = zahlen.Average();
+            int maximalwert = zahlen.Max();
+            int minimalwert = zahlen.Min();
+            Console.WriteLine("Der Durchschnitt ist: " + durchschnitt);
+            Console.WriteLine("Der Minimalwert ist: " + minimalwert);
+            Console.WriteLine("Der Maximalwert ist: " + maximalwert);
+            Console.ReadKey();
 
 
 
